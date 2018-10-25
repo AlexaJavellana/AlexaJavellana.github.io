2ae034183492508d28bf4c2e6f0cef33339e8e0f
Alexa Javellana <AlexaJ@148-100-133-213.FoxNet.marist.edu>
2018-10-25 17:19:18 -0400

    projects pages

--- static/js/script.js
+++ static/js/script.js
@@ -23,27 +23,13 @@ $(function(){
   function projectsShow() {
     $('#projectspg').show();
   }
-  // $('.st1').on("webkitAnimationEnd oanimationend msAnimationEnd animationend", function() {
-  //   $('#aboutArrowID').hover(function() {
-  //     $('#projects').show();
-  //     }, function(){
-  //     $('#projects').hide();  
-  //   });
-  // });
 
-  // $('.st0').on("webkitAnimationEnd oanimationend msAnimationEnd animationend", function(){
-  //   $('#arrowID').hover(function(){
-  //     $('#enter').show();
-  //     }, function(){
-  //     $('#enter').hide();
-  //   });
-  // });
   $('#aboutArrowID').hover(function() {
     $('#projects').show();
     }, function(){
     $('#projects').hide();  
   });
-
+  // ibm arrow hovers and click functionality project page 
   $('#ibm').click(function() {
     $('#projectspg').hide();
     ibmShow();
@@ -53,15 +39,47 @@ $(function(){
     $('.ibmPg').show();
   }
 
+  $('#ibmleftArrow').hover(function() {
+    $('#ibmHome').show();
+    }, function(){
+    $('#ibmHome').hide();  
+  });
+
+  $('#ibmleftArrow').click(function() {
+    $('.ibmPg').hide();
+    projectsShow();
+  });
+
+  $('#ibmrightArrow').hover(function() {
+    $('#ibmNext').show();
+    }, function(){
+    $('#ibmNext').hide();  
+  });
+
+  $('#ibmrightArrow').click(function() {
+    $('.ibmPg').hide();
+    serenityShow();
+  });
+  // end ibm functionality project page
   $('#serenity').click(function() {
     $('#projectspg').hide();
-    ibmShow();
+    serenityShow();
   });
 
   function serenityShow() {
-    $('#serenityPg').show();
+    $('.serenityPg').show();
   }
 
+  $('#serenityleftArrow').click(function() {
+    $('.serenityPg').hide();
+    ibmShow();
+  });
+
+  $('#serenityleftArrow').hover(function() {
+    $('#serenityPast').show();
+    }, function(){
+    $('#serenityPast').hide();  
+  });
 
   $('#datacens').click(function() {
     location.href = "test.html";
